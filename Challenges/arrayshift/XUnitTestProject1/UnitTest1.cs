13d9bd24aee2f2f42abb912249ffd57a7dd2d74f
xia LIU <xia.liu2@outlook.com>
2019-01-09 10:03:06 -0800

    created xunit test for code challenge and updtaed my readme file added link to arrayshift

---
+++ Challenges/arrayshift/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,51 @@
+using System;
+using Xunit;
+using arrayshift;
+
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int[] test = { 2, 4, 6, 8 };
+            int value = 5;
+            int[] expected = { 2, 4, 5, 6, 8 };
+
+            Assert.Equal(expected, Program.ArrayShift(test,value));
+
+
+        }
+
+
+
+
+        [Fact]
+        public void Test2()
+        {
+            int[] test = { 4,8,15,23,42};
+            int value = 16;
+            int[] expected = { 4,8,15,16,23,42 };
+
+            Assert.Equal(expected, Program.ArrayShift(test, value));
+
+
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] test = { 2 };
+            int value = 5;
+            int[] expected = { 2, 5 };
+
+            Assert.Equal(expected, Program.ArrayShift(test, value));
+
+
+        }
+
+
+    }
+}
