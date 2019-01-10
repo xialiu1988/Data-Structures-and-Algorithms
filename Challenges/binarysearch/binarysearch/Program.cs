337496c50fe625cde93628d41c94ab2409078fa3
xia LIU <xia.liu2@outlook.com>
2019-01-09 16:22:03 -0800

    setup files

---
+++ Challenges/binarysearch/binarysearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace binarysearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
