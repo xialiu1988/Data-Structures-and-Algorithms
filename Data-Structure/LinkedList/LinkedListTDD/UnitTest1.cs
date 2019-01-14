a7178a42a751dad0df64303f6c0ee5cee4db1732
xia LIU <xia.liu2@outlook.com>
2019-01-13 20:25:25 -0800

    added xunit testing for linked list to check if the includes method can work fine

--- Data-Structure/LinkedList/LinkedListTDD/UnitTest1.cs
+++ Data-Structure/LinkedList/LinkedListTDD/UnitTest1.cs
@@ -7,9 +7,34 @@ namespace LinkedListTDD
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanDetectExsitingValue()
         {
+            LList list = new LList();
+            list.Insert(34);
+     
+            Assert.True(list.Includes(34));
 
+        }
+
+
+
+        [Fact]
+        public void CanDetectNotExsitingValue()
+        {
+            LList list = new LList();
+            list.Insert(34);
+
+            Assert.False(list.Includes(4));
+
+        }
+        [Fact]
+        public void CanDetectIfValueExsits()
+        {
+            LList list = new LList();
+            list.Insert(0);
+            list.Insert(10);
+            list.Insert(38);
+            Assert.True(list.Includes(10));
 
         }
     }
