137f670b100234c67c316ed9bc54f826236046b6
xia LIU <xia.liu2@outlook.com>
2019-01-13 20:13:49 -0800

    setup files and completed the methods working on testing now

---
+++ Data-Structure/LinkedList/LinkedList/Classes/LList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList.Classes
+{
+    public class LList
+    {
+        public Node Head { get; set; }
+        public Node Current { get; set; }
+
+        public void Insert(int value)
+        {
+            Node insertNode = new Node(value);
+            insertNode.Next = Head;
+            Head = insertNode;
+        }
+
+        public bool Includes(int value)
+        {
+            Current = Head;
+            while (Current.Next != null)
+            {
+                if (Current.Value == value)
+                {
+                    return true;
+                }
+                Current = Current.Next;
+
+                
+            }
+            if (Current.Value == value) return true;
+
+
+            return false;
+        }
+     
+
+        public void print()
+        {
+            Current = Head;
+            while (Current != null)
+            {
+                Console.WriteLine(Current.Value);
+                Current = Current.Next;
+            }
+
+        }
+
+    }
+}
---
+++ Data-Structure/LinkedList/LinkedListTDD/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using LinkedList;
+using LinkedList.Classes;
+namespace LinkedListTDD
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+
+        }
+    }
+}
