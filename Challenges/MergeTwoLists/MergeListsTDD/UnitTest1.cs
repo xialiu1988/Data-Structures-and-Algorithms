fa92c823e999ed950500075ff4808c0de9cc05c1
xia LIU <xia.liu2@outlook.com>
2019-01-17 08:12:32 -0800

    updated merge method

---
+++ Challenges/MergeTwoLists/MergeListsTDD/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+using MergeTwoLists;
+namespace MergeListsTDD
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
