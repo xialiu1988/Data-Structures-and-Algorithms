676ed67d4f5397ef1105245ddd58ddd83fe45ec2
xia LIU <xia.liu2@outlook.com>
2019-01-22 22:08:40 -0800

    created queueWithStacks method out of main method

--- Challenges/QueuewithStacks/QueuewithStacks/Program.cs
+++ Challenges/QueuewithStacks/QueuewithStacks/Program.cs
@@ -10,22 +10,27 @@ namespace QueueWithStacks
         {
             Console.WriteLine("Hello World!");
 
+            QueueWithStacks();
+
+        }
+       public static void QueueWithStacks()
+        {
             Node node1 = new Node(4);
             Stack stack = new Stack(node1);
             stack.Push(20);
             stack.Push(32);
             stack.Push(2);
-           stack.Push(14);
-             PseudoQueue pse = new PseudoQueue(stack);
-            pse.Print(stack);           
+            stack.Push(14);
+            PseudoQueue pse = new PseudoQueue(stack);
+            pse.Print(stack);
             pse.Enqueque(68);
-           // pse.Front = stack;
+            // pse.Front = stack;
             Console.WriteLine("+++++++++++++++++");
             pse.Print(stack);
             Console.WriteLine("+++++++++++++++++");
             Console.WriteLine("afterDEqueue");
             pse.Dequeue();
-           // pse.Front = stack;
+            // pse.Front = stack;
             pse.Print(stack);
             Console.WriteLine("+++++++++++++++++");
             Console.WriteLine("afterDEqueue");
