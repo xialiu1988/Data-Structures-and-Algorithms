bcde8115f5cb13457c8e0193a1597f82e6b44845
xia LIU <xia.liu2@outlook.com>
2019-01-22 22:04:23 -0800

    updated

--- Challenges/QueuewithStacks/QueuewithStacks/Program.cs
+++ Challenges/QueuewithStacks/QueuewithStacks/Program.cs
@@ -1,37 +1,36 @@
 ﻿using System;
-using QueuewithStacks;
-using static QueuewithStacks.PseudoQueue;
+using QueueWithStacks;
+using static QueueWithStacks.PseudoQueue;
 
-namespace QueuewithStacks
+namespace QueueWithStacks
 {
-    public class Program
+  public  class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
 
             Node node1 = new Node(4);
             Stack stack = new Stack(node1);
-            //Stack stack = new Stack();
             stack.Push(20);
             stack.Push(32);
             stack.Push(2);
-            stack.Push(14);
-            PseudoQueue pse = new PseudoQueue(stack);
-            pse.Print(stack);
+           stack.Push(14);
+             PseudoQueue pse = new PseudoQueue(stack);
+            pse.Print(stack);           
             pse.Enqueque(68);
-
+           // pse.Front = stack;
             Console.WriteLine("+++++++++++++++++");
             pse.Print(stack);
             Console.WriteLine("+++++++++++++++++");
             Console.WriteLine("afterDEqueue");
             pse.Dequeue();
-
-           pse.Print(stack);
-            //Console.WriteLine("+++++++++++++++++");
-            //Console.WriteLine("afterDEqueue");
-            //pse.Dequeue();
-            //pse.Print(stack);
+           // pse.Front = stack;
+            pse.Print(stack);
+            Console.WriteLine("+++++++++++++++++");
+            Console.WriteLine("afterDEqueue");
+            pse.Dequeue();
+            pse.Print(stack);
 
 
         }
