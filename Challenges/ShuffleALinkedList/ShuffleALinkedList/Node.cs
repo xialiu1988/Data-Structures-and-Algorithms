a2b10d13c6a3c5ab55bec8dff3aeebdbea5c542e
xia LIU <xia.liu2@outlook.com>
2019-02-11 19:35:08 -0800

    completed shuffle linked list

---
+++ Challenges/ShuffleALinkedList/ShuffleALinkedList/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShuffleALinkedList
+{
+   public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int val)
+        {
+            Value = val;
+        }
+    }
+}
---
+++ Challenges/ShuffleALinkedList/ShuffleALinkedList/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShuffleALinkedList
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            LList l = GetAlist();
+            l.print();
+           // Console.WriteLine(l.counter);
+            Console.WriteLine("====================");
+            ShuffleALinkedList(l);           
+        }
+
+
+        public static LList GetAlist()
+        {
+           
+            LList l = new LList();
+            l.Head = new Node(6);
+           l.counter++;
+            l.Insert(9);       
+            l.Insert(20);        
+            l.Insert(17);
+            l.Insert(23);
+            l.Insert(2);
+            return l;
+            
+        }
+
+        public static void ShuffleALinkedList(LList l)
+        {
+            int[] arr = new int[l.counter];
+            l.Current = l.Head;
+           for(int i = 0; i < l.counter; i++)
+            {
+                arr[i] = l.Current.Value;
+                l.Current = l.Current.Next;
+            }
+         
+       
+           for(int i = 0; i < l.counter / 2; i++)
+            {
+                int temp = 0;
+                temp = arr[i];
+                arr[i] = arr[l.counter - i - 1];
+                arr[l.counter - i - 1] = temp;
+            }
+
+
+
+           for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} =>");
+
+            }
+            Console.WriteLine("null");
+            }
+          
+
+
+        }
+
+
+
+    }
+
